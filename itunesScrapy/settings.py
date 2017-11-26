7ac97e5f2584cf3364a460e234aa3e8fba87f69d
Yu Lu <yulu@utexas.edu>
2017-11-26 17:15:30 -0600

    add youtube search func

--- itunesScrapy/settings.py
+++ itunesScrapy/settings.py
@@ -20,7 +20,7 @@ FEED_URI = "crawled_data.json"
 #USER_AGENT = 'itunesScrapy (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -28,7 +28,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 30
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
--- itunesScrapy/spiders/itunes_song.py
+++ itunesScrapy/spiders/itunes_song.py
@@ -7,13 +7,15 @@ class ItunesSongSpider(scrapy.Spider):
     #allowed_domains = ['https://www.apple.com/itunes/charts/songs/']
     start_urls = ['https://www.apple.com/itunes/charts/songs/']
     songs_exist = []
+    
     def parse(self, response):
+        youtube_query_prefix = "https://www.youtube.com/results?search_query="
         
         song = response.css('h3 a::text').extract()
         artist = response.css('h4 a::text').extract()
         rank = response.css('li strong::text').extract()
         albumLink = response.css('li h3 a::attr(href)').extract()
-                
+        
         for item in zip(song, artist, rank, albumLink):
             scraped_info = {
                 'song': item[0],
@@ -25,10 +27,28 @@ class ItunesSongSpider(scrapy.Spider):
                 pass
             else:
                 self.songs_exist.append(item[0])
+
+                request = scrapy.Request(youtube_query_prefix + item[0].replace(' ', '+'), callback = self.parse_youtubeQuery)
+                request.meta['scraped_info'] = scraped_info
+                yield request
+                
                 request = scrapy.Request(item[3], callback=self.parse_albumLink)
                 request.meta['scraped_info'] = scraped_info
                 yield request
-            
+
+
+    def parse_youtubeQuery(self, response):
+        youtube_link_prefix = "https://www.youtube.com"
+        scraped_info = response.meta['scraped_info']
+        youtube_search_title = response.css('h3 a::text').re(scraped_info['song'])
+        for i, title in enumerate(youtube_search_title):
+            if title:
+                youtube_search_link = response.css('h3 a::attr(href)').extract()[i]
+                scraped_info['youtube_link'] = youtube_link_prefix + youtube_search_link
+                yield scraped_info
+            else:
+                pass
+        
         
     def parse_albumLink(self, response):
         scraped_info = response.meta['scraped_info']
@@ -42,3 +62,9 @@ class ItunesSongSpider(scrapy.Spider):
         scraped_info['genre'] = genre
         scraped_info['release_time'] = release_time
         yield scraped_info
+
+    
+
+            
+            
+            
